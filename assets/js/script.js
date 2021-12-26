9d37aded3612901cb532d2d78d6af0ac2177951d
lesliek_x07 <lesliek.x1999@gmail.com>
2021-12-25 22:16:28 -0300

    Se agregan tooltips y enlaces a redes sociales

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,8 @@ function clickHandler(e) {
         behavior: "smooth"
     });
 }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
